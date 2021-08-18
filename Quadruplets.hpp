f50b8b7e894017032743fcc14b3c0048247974ed
Runer53248 <runer53248@gmail.com>
2021-08-18 15:52:45 +0200

    Fix poker check in quadruplets.

--- Quadruplets.hpp
+++ Quadruplets.hpp
@@ -5,5 +5,6 @@
 DicePoker quadruplets(  //4dices or more
     "quadruplets",
     {NextState(poker, 5, [](auto dices) {
-		return std::accumulate(dices.cbegin(), dices.cend(), 0) % 5 == 0;
+        return *dices.cbegin() == *dices.crbegin();
+		//return std::accumulate(dices.cbegin(), dices.cend(), 0) % 5 == 0;
     })});
