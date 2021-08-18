0c1fb8547b110bb4445e0b245486da5bd9128572
Gyrydymptum <gyrydymptum@gmail.com>
2021-08-18 14:48:43 +0200

    add file.close

--- main.cpp
+++ main.cpp
@@ -9,27 +9,30 @@ void print(std::string str) {
     std::copy(str.begin(), str.end(), std::ostream_iterator<char>(std::cout));
 }
 
-
-
 int main() {
-
-        std::ifstream file("Sumfing.txt");
-        std::string str;
-        std::string file_contents;
-        if (file.is_open()){
-                while (std::getline(file, str)){
-                        file_contents += str;
-                        file_contents.push_back('\n');
-                }
+    std::ifstream file("Sumfing.txt");
+    std::string str;
+    std::string file_contents;
+    if (file.is_open()) {
+        while (std::getline(file, str)) {
+            file_contents += str;
+            file_contents.push_back('\n');
         }
-        std::cout << "ORIGINAL TEXT:\n";
-        print(file_contents);
+    }
+    file.close();
+    std::cout << "ORIGINAL TEXT:\n";
+    print(file_contents);
 
-        auto it = std::unique(file_contents.begin(), file_contents.end(), [](auto& lhs, auto& rhs) {return lhs == rhs && std::isspace(lhs);});
-        file_contents.erase(it, file_contents.end());
+    // auto it = std::unique(file_contents.begin(), file_contents.end(), [](auto& lhs, auto& rhs) {return lhs == rhs && std::isspace(lhs);});
+    // file_contents.erase(it, file_contents.end());
 
-        std::cout << "CONVERTED TEXT:\n";
-        print(file_contents);
+    file_contents.erase(std::unique(file_contents.begin(), file_contents.end(), [](auto& lhs, auto& rhs) {
+                            return lhs == rhs && std::isspace(lhs);
+                        }),
+                        file_contents.end());
 
-        return 0;
+    std::cout << "CONVERTED TEXT:\n";
+    print(file_contents);
+
+    return 0;
 }
