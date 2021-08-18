7365193a318b8dde2d5a49b6bcbbcea62042beb9
Martyna-Tu <80097360+Martyna-Tu@users.noreply.github.com>
2021-08-18 04:49:34 -0400

    first working version

--- main.cpp
+++ main.cpp
@@ -1,1 +1,35 @@
+#include <algorithm>
+#include <cctype>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <string>
 
+void print(std::string str) {
+    std::copy(str.begin(), str.end(), std::ostream_iterator<char>(std::cout));
+}
+
+
+
+int main() {
+
+        std::ifstream file("Sumfing.txt");
+        std::string str;
+        std::string file_contents;
+        if (file.is_open()){
+                while (std::getline(file, str)){
+                        file_contents += str;
+                        file_contents.push_back('\n');
+                }
+        }
+        std::cout << "ORIGINAL TEXT:\n";
+        print(file_contents);
+
+        auto it = std::unique(file_contents.begin(), file_contents.end(), [](auto& lhs, auto& rhs) {return lhs == rhs && std::isspace(lhs);});
+        file_contents.erase(it, file_contents.end());
+
+        std::cout << "CONVERTED TEXT:\n";
+        print(file_contents);
+
+        return 0;
+}
