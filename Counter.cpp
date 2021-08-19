81ce1eaef6dea942545ec0d54d14b580d428e427
Piotr Rogalski <progalski89@gmail.com>
2021-08-19 12:27:48 +0200

    Got rid of loops in print

--- Counter.cpp
+++ Counter.cpp
@@ -7,33 +7,62 @@
 #include <set>
 #include <sstream>
 #include <stdexcept>
+#include <utility>
+#include <vector>
 
 Counter::Counter(const std::string& filePath) {
     std::ifstream inFile{filePath};
     if (!inFile) {
         throw std::invalid_argument("Could not open the file.");
     }
-    calculate(inFile);
+    readFromFile(inFile);
     inFile.close();
     print();
 }
 
-void Counter::calculate(const std::ifstream& inFile) {
+void Counter::readFromFile(const std::ifstream& inFile) {
     std::stringstream ss;
     ss << inFile.rdbuf();
+    WordCounterMapType wordCounterMap;
     std::string word;
     while (ss >> word) {
         word.erase(std::remove_if(word.begin(), word.end(), [](auto c) { return std::ispunct(c); }), word.end());
         std::transform(word.begin(), word.end(), word.begin(), [](auto c) { return std::tolower(c); });
         if (!word.empty()) {
             wordSizeMap_[word.size()].insert(word);
-            ++wordCounterMap_[word];
+            ++wordCounterMap[word];
         }
         ++wordCount_;
     }
-    std::string fileContent {ss.str()};
+    std::string fileContent{ss.str()};
     characterCount_ = fileContent.size();
     lineCount_ = static_cast<size_t>(std::count(fileContent.cbegin(), fileContent.cend(), '\n'));
+    minMaxCounts_ = rareCommonWordsFill(wordCounterMap);
+}
+
+std::pair<size_t, size_t> Counter::rareCommonWordsFill(const WordCounterMapType& map) {
+    auto minmaxVal = std::minmax_element(map.cbegin(), map.cend(),
+                                         [](const auto& a, const auto& b) {
+                                             return a.second < b.second;
+                                         });
+    std::vector<std::pair<std::string, size_t>> rareWordsPair;
+    std::copy_if(map.cbegin(), map.cend(), std::back_inserter(rareWordsPair), [&](const auto& pair) {
+        return pair.second == minmaxVal.first->second;
+    });
+    rareWords_.reserve(rareWordsPair.size());
+    std::transform(rareWordsPair.cbegin(), rareWordsPair.cend(), std::back_inserter(rareWords_), [](const auto& word) {
+        return word.first;
+    });
+
+    std::vector<std::pair<std::string, size_t>> commonWordsPair;
+    std::copy_if(map.cbegin(), map.cend(), std::back_inserter(commonWordsPair), [&](const auto& pair) {
+        return pair.second == minmaxVal.second->second;
+    });
+    commonWords_.reserve(commonWordsPair.size());
+    std::transform(commonWordsPair.cbegin(), commonWordsPair.cend(), std::back_inserter(commonWords_), [](const auto& word) {
+        return word.first;
+    });
+    return std::make_pair(minmaxVal.first->second, minmaxVal.second->second);
 }
 
 void Counter::print() {
@@ -48,21 +77,9 @@ void Counter::print() {
     std::copy(wordSizeMap_.rbegin()->second.cbegin(),
               wordSizeMap_.rbegin()->second.cend(),
               std::ostream_iterator<std::string>(std::cout, " "));
-    auto minmaxVal = std::minmax_element(wordCounterMap_.cbegin(), wordCounterMap_.cend(),
-                                         [](const auto& a, const auto& b) {
-                                             return a.second < b.second;
-                                         });
-    std::cout << "\nRarest words (occurrences:  " << minmaxVal.first->second << "): ";
-    std::for_each(wordCounterMap_.cbegin(), wordCounterMap_.cend(), [&](const auto& pair) {
-        if (pair.second == minmaxVal.first->second) {
-            std::cout << pair.first << ' ';
-        }
-    });
-    std::cout << "\nMost common words (occurrences:  " << minmaxVal.second->second << "): ";
-    std::for_each(wordCounterMap_.cbegin(), wordCounterMap_.cend(), [&](const auto& pair) {
-        if (pair.second == minmaxVal.second->second) {
-            std::cout << pair.first << ' ';
-        }
-    });
+    std::cout << "\nRarest words (occurrences:  " << minMaxCounts_.first << "): ";
+    std::copy(rareWords_.cbegin(), rareWords_.cend(), std::ostream_iterator<std::string>(std::cout, " "));
+    std::cout << "\nMost common words (occurrences:  " << minMaxCounts_.second << "): ";
+    std::copy(commonWords_.cbegin(), commonWords_.cend(), std::ostream_iterator<std::string>(std::cout, " "));
     std::cout << '\n';
 }
--- Counter.hpp
+++ Counter.hpp
@@ -2,6 +2,8 @@
 #include <set>
 #include <sstream>
 #include <string>
+#include <vector>
+#include <utility>
 
 using WordSizeMapType = std::map<size_t, std::set<std::string>>;
 using WordCounterMapType = std::map<std::string, size_t>;
@@ -11,10 +13,13 @@ public:
     Counter(const std::string& filePath);
 
 private:
-    void calculate(const std::ifstream& inFile);
+    void readFromFile(const std::ifstream& inFile);
+    std::pair<size_t, size_t> rareCommonWordsFill(const WordCounterMapType& map);
     void print();
     WordSizeMapType wordSizeMap_;
-    WordCounterMapType wordCounterMap_;
+    std::pair<size_t, size_t> minMaxCounts_;
+    std::vector<std::string> commonWords_;
+    std::vector<std::string> rareWords_;
     size_t characterCount_{0};
     size_t wordCount_{0};
     size_t lineCount_{0};
