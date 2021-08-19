642a4306b40a3b92b8580f497e3691dc9c0920da
Runer53248 <runer53248@gmail.com>
2021-08-19 10:52:22 +0200

    Optimalize state flow.

--- PokerStates/TwoPair.hpp
+++ PokerStates/TwoPair.hpp
@@ -1,15 +1,2 @@
 #pragma once
-DicePoker twoPair(  //4dices or more
-    "two pairs",
-    {
-        NextState(
-            full, 
-            5, 
-            [](const auto& dices) {
-                auto it = std::search_n(dices.cbegin(), dices.cend(), 3, *dices.cbegin());
-                auto it2 = std::search_n(dices.crbegin(), dices.crend(), 3, *dices.crbegin());
-                return it != dices.cend() || it2 != dices.crend();
-            }
-        )
-    }
-);
+DicePoker twoPair("two pairs", {}); //4dices or more
