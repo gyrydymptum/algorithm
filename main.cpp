c7b4f7ca7de0cb047316f19c2c4ef33863386d74
Gyrydymptum <gyrydymptum@gmail.com>
2021-08-19 10:33:28 +0200

    change of loading file

--- main.cpp
+++ main.cpp
@@ -3,24 +3,38 @@
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include <sstream>
 #include <string>
 
 void print(std::string str) {
     std::copy(str.begin(), str.end(), std::ostream_iterator<char>(std::cout));
+    std::cout << '\n';
 }
 
-std::string readTextFromFile(std::string filetxt) {
-    std::ifstream file(filetxt);
-    std::string str;
+// std::string readTextFromFile(std::string filetxt) {
+//     std::ifstream file(filetxt);
+//     std::string str;
+//     std::string file_contents;
+//     if (file.is_open()) {
+//         while (std::getline(file, str)) {
+//             file_contents += str;
+//             file_contents.push_back('\n');
+//         }
+//         file.close();
+//     }
+//     return file_contents;
+// }
+std::string readTextFromFile(const std::string& filetxt) {
+    std::ifstream inFile{filetxt};
+    std::stringstream str;
     std::string file_contents;
-    if (file.is_open()) {
-        while (std::getline(file, str)) {
-            file_contents += str;
-            file_contents.push_back('\n');
-        }
-        file.close();
+    if (!inFile) {
+        throw std::invalid_argument("Could not open the file.");
     }
-    return file_contents;
+    str << inFile.rdbuf();
+    inFile.close();
+    file_contents = str.str();
+  return file_contents;
 }
 
 void convertText(std::string filetxt) {
@@ -40,3 +54,4 @@ int main() {
 
     return 0;
 }
+
